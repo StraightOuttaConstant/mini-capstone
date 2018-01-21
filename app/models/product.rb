3a930958c53569469ee4d4185ec354a658b2025d
JackMarx <seriousfools@gmail.com>
2018-01-21 12:02:04 -0600

    model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ApplicationRecord
+  def discounted?
+    price < 50
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    tax + price
+  end
+
+  def as_json
+    {
+      id: id,
+      name: name,
+      image_url: image_url,
+      description: description,
+      price: price,
+      is_discounted: discounted?,
+      tax: tax,
+      total: total
+    }
+  end
 end
