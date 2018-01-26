aa8152348b799d6c43f4ce42f0e669e7e4e9bfc9
JackMarx <seriousfools@gmail.com>
2018-01-25 18:42:17 -0600

    jbuilder

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,40 +1,51 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    @products = Product.all
+
+    search_term = params[:search]
+    if search_term
+      @products = @products.where("name iLike ?", "%#{search_term}%")
+    end
+
+    sort_attribute = params[:sort]
+    if sort_attribute
+      @products = @products.order(sort_attribute => :asc)
+    end
+
+    render 'index.json.jbuilder'
   end
 
   def create
-    product = Product.new(
+    @product = Product.new(
                           name: params[:name],
                           description: params[:description],
                           price: params[:price],
                           image_url: params[:image_url]
                           )
-    if product.save
-      render json: product.as_json
+    if @product.save
+      render 'show.json.jbuilder'
     else
-      render json: {errors: product.errors.full_messages}, status: :unprocessable_entity
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   def show
-    product = Product.find(params[:id])
-    render json: product.as_json
+    @product = Product.find(params[:id])
+    render 'show.json.jbuilder'
   end
 
   def update
-    product = Product.find(params[:id])
+    @product = Product.find(params[:id])
     
-    product.name = params[:name] || product.name
-    product.description = params[:description] || product.description
-    product.price = params[:price] || product.price
-    product.image_url = params[:image_url] || product.image_url
+    @product.name = params[:name] || @product.name
+    @product.description = params[:description] || @product.description
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
     
-    if product.save
-      render json: product.as_json
+    if @product.save
+      render 'show.json.jbuilder'
     else
-      render json: {message: product.errors.full_messages}, status: :unprocessable_entity
+      render json: {message: @product.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -16,17 +16,4 @@ class Product < ApplicationRecord
   def total
     tax + price
   end
-
-  def as_json
-    {
-      id: id,
-      name: name,
-      image_url: image_url,
-      description: description,
-      price: price,
-      is_discounted: discounted?,
-      tax: tax,
-      total: total
-    }
-  end
 end
