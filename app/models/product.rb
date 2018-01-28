74c0fe1ed1e6c12c1e4ba4c214fd0b389b29285a
JackMarx <seriousfools@gmail.com>
2018-01-28 15:02:53 -0600

    supplier restful

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  belongs_to :supplier
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, presence: true
@@ -18,61 +20,6 @@ class Product < ApplicationRecord
   end
 end
 
-
-
-
-
-puts <<-REPTAR
-                                               MMMM        .NMMM.
-                                               MMMMMM     =NMMMM.               
-                                              MNMMMMMMMNMMMMMMMM.
-                                              ,NMMMMMMMMMMMMMMMMMM.
-                                               MMMMMMMMMMMMMMMMMMMMMMMM.
-                                             ,MMMMMMMMMMMMMMMMMMMMMMMMM 
-                                       ,MMMM MMMMMMMMMMMMMMMMMMMMMMMMMM 
-                                        MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.
-                                       ,MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN8.
-                                        .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM 
-                                         .MMMMMMMMMMMMM,MMMMMMMMMMMMMMMMMMMNMMMM
-                                       ..MMMMMMMMMMMMMMMMMM :+.DMMMMMMMMMMMMMMN 
-                                    ....MMMMMMMMMMMMMMMMMM.  ...D.MDMMMMMMMMMMN.
-                              . . NN:M. MMMMMMMMMMMMMMMMMM.       ..,.. .MMMMM..
-                             . MNMMMMMMMMMMMMMMMMMMMMMMMMMM              .. ,   
-                              ..MMMMMMMMMMMMMMMMMMM.,MMMMMM  .                  
-                                ,MMMMMMMMMMMMMMMMMMMMMMM MMNNM .:         Do You Really Want To Write This Code?       
-                                 .MNMMMMMMMMMMMMMMMMMMMMMMMM.MDDM 7             
-                                   .MMMMMMMMMMMMMMMMMMMMMMMMMMMMN8NM?           
-                                   .MMMMMMMMMMMMMMMMMMMMM.NNMNMMMM~DO$.         
-                                 . MMMMMMMMMMMMMMMMMMMMMI.   ..$MMNMM8.         
-                            .MM?,.:MMMMMMMMMMMMMMMMMMMMMNM....    :$...         
-                          .NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.                   
-                          .MNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM                   
-                          .MMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMM .                   
-                            ..MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM,.                 
-                                MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMZ:                
-                               .MMMMMMMMMMMMMMMMMMMMMMMM...MMMMM.               
-                                MMMMMMMMMMMMMMMMMMMMMMMM:.=MMMMNM               
-                         .     ZMMMMMMMMMMMMMMMMMMMMMMMM: $MMNMMM               
-...................... .NMMM..MMMMMMMMMMMMMMMMMMMMMMMMMMM..$I8M??...............
-                       .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM  .                    
-                       .DNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM                       
-                         ,MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.                      
-                           MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNM                     
-                   . .   .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMN88                   
-  .              .DDDI . DMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNND                  
-M8  .            ~NDDDM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM                  
-MMMM8+.          NDNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM .                
-MMMDM    M8MDM    .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM .                
-MMMMM?  . MNNM,..MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN. ................
- MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN?. ................
-. NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM ...................
-. +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMM.MMMMMMMMMMM .....................
- . .MMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMMMMM~: .....DMMMMMMMMMNNNNMMN.... .  . .. . 
- . .. MNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMMMMMMMMMMMMMMMMMMMMMMM .. .  . .. . 
-      ...MMMMMMMNNDMMMMMMM. .MMMMMMMMMMMMMMMNMMMMMMMMMMMMMMMMMMM~,.             
-            .. . .   .       7MMMMMMMMMMNMMMMNMMM..      ...   .                
-                            .          . .M..  ....                             
-REPTAR
 
 
 
