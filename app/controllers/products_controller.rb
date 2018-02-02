1eb9011186d1394c9fcd474ce34709d0de9c17e0
JackMarx <seriousfools@gmail.com>
2018-02-01 20:47:08 -0600

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_admin, only: [:create, :update, :destroy]
+  # before_action :authenticate_admin, except: [:index, :show]
+
   def index
     @products = Product.all
 
@@ -12,25 +15,26 @@ class ProductsController < ApplicationController
       @products = @products.order(sort_attribute => :asc)
     end
 
+    category_name = params[:category]
+    if category_name
+      @products = Category.find_by(name: category_name).products
+    end
+
     render 'index.json.jbuilder'
   end
 
   def create
-    if current_user && current_user.admin
-      @product = Product.new(
-                            name: params[:name],
-                            description: params[:description],
-                            price: params[:price],
-                            supplier_id: params[:supplier_id]
-                            )
-      
-      if @product.save
-        render 'show.json.jbuilder'
-      else
-        render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
-      end
+    @product = Product.new(
+                          name: params[:name],
+                          description: params[:description],
+                          price: params[:price],
+                          supplier_id: params[:supplier_id]
+                          )
+    
+    if @product.save
+      render 'show.json.jbuilder'
     else
-      render json: {message: "you are not authorized"}, status: :unauthorized
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
@@ -40,31 +44,23 @@ class ProductsController < ApplicationController
   end
 
   def update
-    if current_user && current_user.admin
-      @product = Product.find(params[:id])
-      
-      @product.name = params[:name] || @product.name
-      @product.description = params[:description] || @product.description
-      @product.price = params[:price] || @product.price
-      @product.supplier_id = params[:supplier_id] || @product.supplier_id
-      
-      if @product.save
-        render 'show.json.jbuilder'
-      else
-        render json: {message: @product.errors.full_messages}, status: :unprocessable_entity
-      end
+    @product = Product.find(params[:id])
+    
+    @product.name = params[:name] || @product.name
+    @product.description = params[:description] || @product.description
+    @product.price = params[:price] || @product.price
+    @product.supplier_id = params[:supplier_id] || @product.supplier_id
+    
+    if @product.save
+      render 'show.json.jbuilder'
     else
-      render json: {message: "you are not authorized"}, status: :unauthorized
+      render json: {message: @product.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   def destroy
-    if current_user && current_user.admin
-      product = Product.find(params[:id])
-      product.destroy
-      render json: {message: "Successfully destroyed product ##{product.id}"}
-    else
-      render json: {message: "you are not authorized"}, status: :unauthorized
-    end
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Successfully destroyed product ##{product.id}"}
   end
 end
